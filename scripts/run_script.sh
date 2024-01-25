8a756b0fd217e99d9cf6c75ca8d67887a1ad5a68
Rafael Papallas <rpapallas@gmail.com>
2024-01-25 10:02:51 +0000

    Fixed issue with runscript and path after build.

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -11,6 +11,7 @@ if [ ! -d "$HOME/mujoco_mpc/build" ]; then
     mkdir build && cd build
     cmake .. -DCMAKE_BUILD_TYPE:STRING=Release -G Ninja -DCMAKE_C_COMPILER:STRING=clang-12 -DCMAKE_CXX_COMPILER:STRING=clang++-12 -DMJPC_BUILD_GRPC_SERVICE:BOOL=ON
     cmake --build . --target mjpc agent_test cost_derivatives_test norm_test rollout_test threadpool_test trajectory_test utilities_test direct_force_test direct_optimize_test direct_parameter_test direct_sensor_test direct_trajectory_test direct_utilities_test batch_filter_test batch_prior_test kalman_test unscented_test agent_server direct_server filter_server
+    cd $HOME
 fi
 
 ./mujoco_mpc/build/bin/mjpc
